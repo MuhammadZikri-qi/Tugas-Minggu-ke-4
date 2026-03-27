6bc1aa3668e8d7b1f6a240724dcf239e3ad6c6c4
Muhammad Zikri <muhammadzikri021106@gmail.com>
2026-03-27 10:16:29 +0700

    Create Soal8.java

---
+++ Soal8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Soal8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan ukuran matriks (contoh: 5): ");
+        int n = input.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1) {
+                    System.out.print("0");
+                } else if (i == j) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("x");
+                }
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
