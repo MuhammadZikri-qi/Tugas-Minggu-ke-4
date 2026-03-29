24cfb69452b6043448eba22f5d9bef07bd93aaf1
Muhammad Zikri <muhammadzikri021106@gmail.com>
2026-03-29 19:56:07 +0700

    Add input prompt for matrix size
    Prompt user for matrix size input.

--- Soal08.java
+++ Soal08.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Soal8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         System.out.print("Masukkan ukuran matriks: ");
         int n = input.nextInt();
         
